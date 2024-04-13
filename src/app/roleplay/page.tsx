da0e2c2173f45e48b6b0eeb92ecf7958fc51687b
Ismail Memon <iamismailmemon@gmail.com>
2024-04-13 08:26:16 -0700

    first commit

--- src/app/roleplay/page.tsx
+++ src/app/roleplay/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React, { useState, useEffect } from 'react';
 import roleplaysData from "../../../RolePlay.json";
@@ -8,6 +7,7 @@ function Roleplays() {
     const router = useRouter();
     const [roleplays, setRoleplays] = useState([]);
     const [categorizedRoleplays, setCategorizedRoleplays] = useState([]);
+    const [currentRoleplay, setCurrentRoleplay] = useState(null);
 
     useEffect(() => {
         setRoleplays(roleplaysData);
@@ -39,11 +39,17 @@ function Roleplays() {
         return categorizedRoleplays;
     };
 
+    // Function to handle roleplay click
+    const handleRoleplayClick = (roleplayId) => {
+        router.push(`/roleplay?id=${roleplayId}`)
+        setCurrentRoleplay(roleplays.find(roleplay => roleplay.id === roleplayId));
+    };
+
     return (
         <div>
             <header className='bg-gray-800 flex items-center justify-between h-[80px] w-full px-6'>
                 <div className=''>
-                    <svg onClick={() => { router.push('/dashboard') }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="w-7 h-7 text-white cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
+                    <svg onClick={() => { router.push('/roleplay'); setCurrentRoleplay(null) }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="w-7 h-7 text-white cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                     </svg>
                 </div>
@@ -53,30 +59,62 @@ function Roleplays() {
                     </svg>
                 </div>
             </header>
-            <div className='mx-auto sm:max-w-5xl text-center sm:mt-14 mt-8 sm:text-base text-sm sm:px-2 px-6'>
-                <h2 className='sm:text-5xl text-3xl font-bold'>ROLEPLAYS</h2>
-                <div className='w-full flex justify-center items-center bg-gray-100 rounded-2xl sm:px-14 px-8 sm:py-6 py-3 sm:mt-14 mt-8 sm:text-base text-sm text-black'>
-                    With the Roleplay mode practicing language will be fun and exciting. You can pick various settings from daily conversations to creative and fantastic dialogues.
-                </div>
-            </div>
-            <div className='sm:max-w-[1600px] sm:mx-auto sm:px-2 px-6 sm:mt-16 mt-8'>
-                {categorizedRoleplays.map((category, index) => (
-                    <div key={index}>
-                        <h2 className="text-2xl font-semibold mt-8">{category.name}</h2>
-                        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 sm:gap-12 gap-6 sm:mt-12 mt-8 '>
-                            {category.roleplays.map((roleplay) => (
-                                <div key={roleplay.id}>
-                                    <div key={index} onClick={() => { router.push(`/roleplay?id=${'2333246'}`); }} style={{ backgroundImage: "url('https://app.talkpal.ai/static/media/Chat-Mode.c505bbb6ecbf5c1b429e.jpg')" }} className='transition-transform transform-gpu hover:scale-105 duration-500 h-[150px] rounded-3xl flex justify-center items-center text-white cursor-pointer'>
-                                        <div>
-                                            <h3 className="text-lg font-semibold text-center mt-2">{roleplay.title}</h3>
-                                            <p className="text-sm text-gray-100 text-center">{roleplay.scenario}</p>
-                                        </div>
+
+            <div>
+                {currentRoleplay ? (
+                    <div>
+                        <div style={{ backgroundImage: `url(${currentRoleplay.image})` }} className='w-full h-screen px-2 flex justify-center'>
+                            <div className='sm:py-8 py-6 w-full'>
+                                <h2 className='sm:text-6xl text-center text-4xl text-white font-bold mt-8'>{currentRoleplay.type}</h2>
+                                <div className='sm:mt-20 mt-10 w-full'>
+                                    <h2 className='sm:text-4xl text-center text-2xl font-bold text-white mt-8'>Ordering a taxi:</h2>
+                                    <div className='mt-6 bg-black bg-opacity-40 rounded-lg mx-2 px-3 sm:max-w-4xl sm:mx-auto sm:py-12 py-8'>
+                                       <div className=''>
+                                       <h2 className='text-white text-center sm:text-3xl text-xl font-semibold'>Setting</h2>
+                                        <h4 className='text-white mt-2 text-center sm:text-lg'>{currentRoleplay.setting}</h4>
+                                       </div>
+                                       <div className='sm:pt-12 pt-8'>
+                                       <h2 className='text-white text-center sm:text-3xl text-xl font-semibold'>Scenario</h2>
+                                        <h4 className='text-white mt-2 text-center sm:text-lg'>{currentRoleplay.scenario}</h4>
+                                       </div>
+                                       <div className='sm:pt-12 pt-8'>
+                                       <h2 className='text-white text-center sm:text-3xl text-xl font-semibold'>Your Role</h2>
+                                        <h4 className='text-white mt-2 text-center sm:text-lg'>{currentRoleplay.role}</h4>
+                                       </div>
                                     </div>
                                 </div>
-                            ))}
+                            </div>
                         </div>
+
+
                     </div>
-                ))}
+                ) : (
+                    <div className='sm:max-w-[1600px] sm:mx-auto sm:px-2 px-6 sm:mt-16 mt-8'>
+                        <div className='mx-auto sm:max-w-5xl text-center sm:mt-14 mt-8 sm:text-base text-sm sm:px-2'>
+                            <h2 className='sm:text-5xl text-3xl font-bold'>ROLEPLAYS</h2>
+                            <div className='w-full flex justify-center items-center bg-gray-100 rounded-2xl sm:px-20 px-6 sm:py-6 py-3 sm:mt-14 mt-8 sm:text-base text-sm text-black'>
+                                With the Roleplay mode practicing language will be fun and exciting. You can pick various settings from daily conversations to creative and fantastic dialogues.
+                            </div>
+                        </div>
+                        {categorizedRoleplays.map((category, index) => (
+                            <div key={index}>
+                                <h2 className='sm:text-3xl text-2xl font-bold mt-8'>{category.name}</h2>
+                                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 sm:gap-12 gap-6 sm:mt-12 mt-8 '>
+                                    {category.roleplays.map((roleplay) => (
+                                        <div key={roleplay.id}>
+                                            <div onClick={() => { handleRoleplayClick(roleplay.id); }} style={{ backgroundImage: `url(${roleplay.image})` }} className='transition-transform transform-gpu hover:scale-105 duration-500 sm:h-[150px] h-[130px] rounded-3xl flex justify-center items-center text-white cursor-pointer px-3'>
+                                                <div>
+                                                    <h3 className="text-lg font-semibold text-center mt-2">{roleplay.title}</h3>
+                                                    <p className="text-[13px] mt-1 text-gray-100 text-center">{roleplay.scenario}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
