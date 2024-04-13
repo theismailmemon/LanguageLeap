66f3e5028cce0fe22ccea704887560156ae2dc54
Ismail Memon <iamismailmemon@gmail.com>
2024-04-13 11:02:08 -0700

    first commit

--- src/app/abc/page.tsx
+++ src/app/abc/page.tsx
@@ -135,10 +135,10 @@ const ChatApp = () => {
       <div className="flex-1 h-full overflow-y-auto p-4 flex flex-col gap-5">
         {/* Display chat messages */}
         {messages.map((message, index) => (
-          <MessageRow key={index} message={message} replay={replayMessage} />
+          <MessageRow key={index} message={message} onReplay={replayMessage} />
         ))}
         {/* Display loading message when waiting for response */}
-        {loading && <LoadingMessage loading={true} />}
+        {loading && <LoadingMessage />}
       </div>
 
       {/* Chat input container */}
